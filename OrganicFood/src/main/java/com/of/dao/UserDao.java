c4b4e729fb09db8d1c79eedb6d251485a89033fc
amandhagoriparthi <amandhagoriparthi@192.168.1.24>
2017-07-21 16:19:43 +0530

    OrganicFood HAs connected with backend

---
+++ OrganicFood/src/main/java/com/of/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.of.dao;
+
+import com.of.model.Users;
+
+public interface UserDao {
+
+	public boolean saveUser(Users user);
+
+}
---
+++ OrganicFood/src/main/java/com/of/model/Users.java
@@ -0,0 +1,56 @@
+package com.of.model;
+
+import javax.persistence.*;
+
+
+
+@Entity
+@Table(name="User_Details")
+public class Users{
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String name,email,address,password,role;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
+
